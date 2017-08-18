b608b71d6d681979505d18237a579157df657f53
Luca Stefani <luca020400@lineageos.org>
2017-08-18 10:35:47 +0200

    sepolicy: Initial repo structure

---
+++ common/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all products that
+# inherit from Conquer
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/conquer/sepolicy/common
---
+++ qcom/sepolicy.mk
@@ -0,0 +1,7 @@
+#
+# This policy configuration will be used by all qcom products
+# that inherit from conquer
+#
+
+BOARD_SEPOLICY_DIRS += \
+    device/conquer/sepolicy/qcom
